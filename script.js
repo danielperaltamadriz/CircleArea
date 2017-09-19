3da4eb98a0cf33308eb11004f97f024114d4b94b
danielperaltamadriz <danielperaltamadriz@gmail.com>
2017-09-18 20:15:19 -0600

    clean code - format code

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-(function(){
-
-
-function calculateRadio(){
-    var radio = Number(prompt('Please, write the radio for the circle: '));
-    area = Math.PI * (Math.pow(radio, 2));
-    console.log('The area is: ', area);
-}
-
-return calculateRadio();
+(function () {
+    function calculateRadio() {
+        var radio = Number(prompt('Please, write the radio for the circle: '));
+        area = Math.PI * (Math.pow(radio, 2));
+        console.log('The area is: ', area);
+    }
+    return calculateRadio();
 })()
